081c501f10fb4fadf207d1ebb13af2f161b978db
tom <tom@sjfood.us>
2020-09-21 22:37:18 +0800

    fix: fix test case

--- tests/LUIS/Tests/AppTest.php
+++ tests/LUIS/Tests/AppTest.php
@@ -2,10 +2,33 @@
 
 namespace LUIS\Tests;
 
+use LUIS\LuisClient;
 use LUIS\Models\App;
 
-class AppTest extends FeatureTest
+class AppTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var LuisClient
+     */
+    public $luisClient;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $primaryKey = getenv('LUIS_PRIMARY_KEY');
+        $location = getenv('LUIS_LOCATION');
+
+        $this->luisClient = new LuisClient($primaryKey, $location);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        unset($this->luisClient);
+    }
+
     public function testCreateUpdateDeleteApp()
     {
         $app = (new App())->setName('test')->setDescription('newdescription');
